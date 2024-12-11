2cfffecb5bdb7f3979fe61db07c99425c9dc1357
Jitesh117 <sahookumarjitesh@gmail.com>
2024-12-11 19:49:57 +0530

    feat: bubbleSort, insertionSort, quickSort

---
+++ sorting/sorting.go
@@ -0,0 +1,64 @@
+package sorting
+
+func BubbleSort(arr []int) []int {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
+
+func InsertionSort(arr []int) []int {
+	n := len(arr)
+	for i := 1; i < n; i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+	return arr
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[low]
+	i := low
+	j := high
+
+	for i < j {
+		for arr[i] <= pivot && i <= high-1 {
+			i++
+		}
+
+		for arr[j] > pivot && j >= low+1 {
+			j--
+		}
+
+		if i < j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	arr[low], arr[j] = arr[j], arr[low]
+	return j
+}
+
+func quickSortHelper(arr []int, low, high int) {
+	if low < high {
+		pIndex := partition(arr, low, high)
+
+		quickSortHelper(arr, low, pIndex-1)
+		quickSortHelper(arr, pIndex+1, high)
+	}
+}
+
+func QuickSort(arr []int) []int {
+	quickSortHelper(arr, 0, len(arr)-1)
+	return arr
+}
---
+++ sorting/sorting_test.go
@@ -0,0 +1,77 @@
+package sorting
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "Already sorted array",
+			input:    []int{1, 2, 3, 4, 5},
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Reverse sorted array",
+			input:    []int{5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "Unsorted array with duplicates",
+			input:    []int{3, 1, 2, 1, 3},
+			expected: []int{1, 1, 2, 3, 3},
+		},
+		{
+			name:     "Array with one element",
+			input:    []int{42},
+			expected: []int{42},
+		},
+		{
+			name:     "Empty array",
+			input:    []int{},
+			expected: []int{},
+		},
+		{
+			name:     "Array with negative numbers",
+			input:    []int{-3, -1, -2, -5, -4},
+			expected: []int{-5, -4, -3, -2, -1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := BubbleSort(
+				append([]int{}, tt.input...),
+			)
+			if !reflect.DeepEqual(output, tt.expected) {
+				t.Errorf("BubbleSort(%v) = %v; want %v", tt.input, output, tt.expected)
+			}
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := InsertionSort(
+				append([]int{}, tt.input...),
+			)
+			if !reflect.DeepEqual(output, tt.expected) {
+				t.Errorf("BubbleSort(%v) = %v; want %v", tt.input, output, tt.expected)
+			}
+		})
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := QuickSort(
+				append([]int{}, tt.input...),
+			)
+			if !reflect.DeepEqual(output, tt.expected) {
+				t.Errorf("BubbleSort(%v) = %v; want %v", tt.input, output, tt.expected)
+			}
+		})
+	}
+}
